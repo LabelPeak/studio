8074f4c6e04c78ef095f56ec56175fa1b2bc0285
j10c <blyb1739@gmail.com>
2023-10-16 18:55:46 +0800

    feat(auth): add auth services

---
+++ apps/client/src/services/auth/index.ts
@@ -0,0 +1,19 @@
+import { request } from "../request";
+
+interface ILoginProps {
+  username: string;
+  password: string;
+}
+
+function login(props: ILoginProps) {
+  return request<{ token: string }>("/api/user/login", {
+    method: "POST",
+    body: JSON.stringify(props)
+  });
+}
+
+const AuthService = {
+  login
+};
+
+export default AuthService;
