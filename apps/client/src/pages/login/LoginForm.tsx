ed3596ebe5cf64510a320c75c09e4ef7ea818a50
j10c <blyb1739@gmail.com>
2023-10-16 18:57:44 +0800

    feat(login): complete login request logic

--- apps/client/src/pages/login/LoginForm.tsx
+++ apps/client/src/pages/login/LoginForm.tsx
@@ -1,5 +1,8 @@
 import { Button, Form, Input } from "antd";
+import AuthService from "@/services/Auth";
 import { type IntlShape } from "react-intl";
+import useAuth from "@/hooks/useAuth";
+import { useNavigate } from "react-router-dom";
 
 interface IProps {
   intl: IntlShape
@@ -7,9 +10,26 @@ interface IProps {
 
 export default function LoginForm(props: IProps) {
   const { intl } = props;
+  const { setAuthConfig } = useAuth();
+  const navigate = useNavigate();
+
+  async function handleLogin(e: {username: string, password: string}) {
+    const { username, password } = e;
+    const res = await AuthService.login({ username, password });
+    if (res.data) {
+      const { token } = res.data;
+      setAuthConfig({ token });
+      navigate("/dashboard");
+    }
+  }
 
   return (
-    <Form name="login" style={{ width: "360px" }} size="large">
+    <Form
+      name="login"
+      style={{ width: "360px" }}
+      size="large"
+      onFinish={handleLogin}
+    >
       <Form.Item name="username">
         <Input placeholder={ intl.formatMessage({id: "username-prompt"})}/>
       </Form.Item>
@@ -19,7 +39,7 @@ export default function LoginForm(props: IProps) {
           autoComplete="on"
         />
       </Form.Item>
-      <Form.Item >
+      <Form.Item>
         <Button type="primary" htmlType="submit" block>
           { intl.formatMessage({ id: "login" })}
         </Button>
