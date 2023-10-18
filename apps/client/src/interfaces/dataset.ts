2b1d27c1b511ac4302ba13fbe5c30a1130b4ba49
j10c <blyb1739@gmail.com>
2023-10-18 19:55:00 +0800

    feat(project/service): detail and dataset services

---
+++ apps/client/src/interfaces/dataset.ts
@@ -0,0 +1,23 @@
+export enum DataType {
+  Picture = "picture",
+  Text = "text",
+}
+
+export interface Dataset {
+  id: number;
+  /** 文件地址 */
+  location: string;
+  /** 所属项目的id */
+  project: number;
+  type: DataType;
+}
+
+export interface DataItem {
+  id: number;
+  /** JSON 格式的标注信息 */
+  annotation: string;
+  /** 所属的数据集id */
+  dataset: number;
+  /** 文件名称 */
+  file: string;
+}
