49e77589642f60b2c4c0ed32abcbd1e8cd3de952
j10c <blyb1739@gmail.com>
2023-10-19 12:24:55 +0800

    fix(layout): project header go back

--- apps/client/src/pages/project/[id]/ProjectHeader.tsx
+++ apps/client/src/pages/project/[id]/ProjectHeader.tsx
@@ -1,5 +1,5 @@
-import { Link, useNavigate } from "react-router-dom";
-import { type ReactNode, memo } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { type ReactNode, memo, useMemo } from "react";
 import { Project } from "@/interfaces/project";
 
 interface IProps {
@@ -10,13 +10,19 @@ interface IProps {
 function ProjectHeader(props: IProps) {
   const { project, extra } = props;
   const navigate = useNavigate();
+  const location = useLocation();
+  const backPathname = useMemo(() => {
+    const temp = location.pathname.split("/");
+    temp.pop();
+    return temp.join("/");
+  }, [location]);
 
   return (
     <div className="h-14 flex b-b-1 b-b-solid b-color-nord-snow-0">
       <Link
         className="b-r-1 b-r-solid b-color-nord-snow-2 px-4 flex items-center hover:bg-nord-snow-2"
         to=".."
-        onClick={(e) => {e.preventDefault(); navigate(-1); } }
+        onClick={(e) => {e.preventDefault(); navigate(backPathname); } }
       >
         <div className="i-mdi-arrow-left text-5 c-nord-polar-3" />
       </Link>
