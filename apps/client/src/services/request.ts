c699b026a9849b51713e13de14d7fa0b6743f522
j10c <blyb1739@gmail.com>
2025-05-05 23:30:55 +0800

    refactor(client): consume response code out of logic

--- apps/client/src/services/request.ts
+++ apps/client/src/services/request.ts
@@ -4,39 +4,42 @@ interface CustomResponse<T> {
   msg?: string;
 }
 
-export async function request<T>(
-  url: string,
-  options?: RequestInit
-) {
-  const res = await fetch(
-    import.meta.env.DEV ? url : import.meta.env.VITE_HOST + url,
-    {
-      ...options,
-      headers: {
-        "Content-Type": "application/json",
-        ...options?.headers,
-      }
-    });
-  const data = await res.json() as CustomResponse<T>;
-  return data;
+export async function request<T>(url: string, options?: RequestInit) {
+  const raw = await fetch(import.meta.env.DEV ? url : import.meta.env.VITE_HOST + url, {
+    ...options,
+    headers: {
+      "Content-Type": "application/json",
+      ...options?.headers
+    }
+  });
+  const res = (await raw.json()) as CustomResponse<T>;
+
+  if (res.code !== 200) {
+    // TODO: shared biz error
+    throw new Error(res.msg || "Unknown Error");
+  }
+
+  return res.data as T;
 }
 
-export function requestWithAuth<T>(
-  url: string,
-  options?: RequestInit
-) {
+export function requestWithAuth<T>(url: string, options?: RequestInit) {
   const context = localStorage.getItem("auth");
   if (!context) {
+    // TODO: shared biz error
     throw new Error("Auth Context Not Found");
   } else {
     const token = JSON.parse(context)?.state?.token;
-    if (!token) throw new Error("Auth Token Invalid");
-    else return request<T>(url, {
-      ...(options || {}),
-      headers: {
-        ...(options || {})?.headers,
-        Authorization: token
-      }
-    });
+    if (!token) {
+      // TODO: shared biz error
+      throw new Error("Auth Token Invalid");
+    } else {
+      return request<T>(url, {
+        ...(options || {}),
+        headers: {
+          ...(options || {}).headers,
+          Authorization: token
+        }
+      });
+    }
   }
 }
