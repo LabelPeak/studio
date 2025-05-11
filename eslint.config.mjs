fde537654743cdff865ca6a16f771d6cad53265b
j10c <blyb1739@gmail.com>
2025-05-11 16:56:54 +0800

    chore(server): js eslint config and remove jiti

---
+++ eslint.config.mjs
@@ -0,0 +1,25 @@
+import zjutjh from "@zjutjh/eslint-config";
+
+export default zjutjh(
+  {
+    ts: {
+      parserOptions: {
+        projectService: {
+          defaultProject: "tsconfig.eslint.json"
+        },
+        project: ["tsconfig.eslint.json", "**/packages/*/tsconfig.json", "**/apps/*/tsconfig.json"]
+      }
+    },
+    prettier: true,
+    overrides: {
+      ts: {
+        "@typescript-eslint/no-unnecessary-condition": "error"
+      }
+    }
+  },
+  {
+    rules: {
+      curly: ["error", "all"]
+    }
+  }
+);
